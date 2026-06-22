0d52623c37c6af77b40fc98ddff8d15f1779866a
dev_vens <vensalex1991@gmail.com>
2026-06-22 16:12:27 +0200

    Add model OrgInstance

---
+++ app/Models/OrgInstance.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class OrgInstance extends Model
+{
+    protected $fillable = [
+        'type',
+        'recurrence_type',
+        'date_meeting',
+        'is_archived'
+    ];
+
+    protected $casts = [
+        'date_meeting' => 'datetime',
+        'is_archived' => 'boolean'
+    ];
+
+    public function tasks(): hasMany{
+        return $this->hasMany(Task::class);
+    }
+}
