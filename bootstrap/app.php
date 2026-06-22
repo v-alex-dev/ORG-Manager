95f7c0ad986e0df4a4f544ff14a6ac43d87c1f75
dev_vens <vensalex1991@gmail.com>
2026-06-22 13:31:34 +0200

    Add exception 404 response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,4 +21,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
+        // 404 - Route or model not found
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            return response()->json([
+                'message' => 'Resource not found.',
+            ], 404);
+        });
     })->create();
