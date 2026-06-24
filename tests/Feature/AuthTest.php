ebbbab33481195e88d14f42b6d588a743c81d393
dev_vens <vensalex1991@gmail.com>
2026-06-24 12:42:19 +0200

    Import user model

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-
+use App\Models\User;
 class AuthTest extends TestCase
 {
     use RefreshDatabase;
