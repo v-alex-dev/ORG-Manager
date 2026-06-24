bbfba3111ddefd245e6138aed5b4d9966d94716e
dev_vens <vensalex1991@gmail.com>
2026-06-24 12:08:04 +0200

    Create public api route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/*
+|--------------------------------------------------------------------------
+| Public routes
+|--------------------------------------------------------------------------
+*/
+Route::post('/login', [AuthController::class, 'login']);
+
+/*
+|--------------------------------------------------------------------------
+| Protected routes (require a valid Sanctum token)
+|--------------------------------------------------------------------------
+*/
