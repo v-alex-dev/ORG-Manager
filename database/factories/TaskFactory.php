38c78c65c31448321e5fc184986fea1cc063bf23
dev_vens <vensalex1991@gmail.com>
2026-07-08 18:56:08 +0200

    Add TaskFactory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\OrgInstance;
+use App\Models\Service;
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'org_instance_id' => OrgInstance::factory(),
+            'service_id' => Service::factory(),
+            'poj_title' => fake()->sentence(4),
+            'task_description' => fake()->paragraph(),
+            'status' => 'TODO',
+            'reference_code' => 'CFG-2026-' . str_pad(fake()->unique()->numberBetween(1, 999), 3, '0', STR_PAD_LEFT),
+        ];
+    }
+
+    public function done(): static
+    {
+        return $this->state(['status' => 'DONE']);
+    }
+}
