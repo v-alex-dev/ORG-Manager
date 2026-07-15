8a2a0fc9b8d6bc50d2094b261ccfbf963b08f47a
dev_vens <vensalex1991@gmail.com>
2026-07-15 12:21:52 +0200

    Create add refresh data for testing tasks

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,19 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Models\OrgInstance;
+use App\Models\Service;
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
+    use refreshDatabase;
 }
