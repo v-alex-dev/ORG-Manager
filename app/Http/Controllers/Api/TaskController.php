9f623d39f668f10627b45dd3b50a1f1ccff46214
dev_vens <vensalex1991@gmail.com>
2026-07-15 10:57:23 +0200

    Create task controller & add multiple use for controller

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\OrgInstance;
+use App\Models\Task;
+use App\Services\ReferenceCodeService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class TaskController extends Controller
+{
+    //
+}
