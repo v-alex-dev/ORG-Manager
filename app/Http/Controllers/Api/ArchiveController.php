547017b69e54ef1f335838c9360845526d347ed2
dev_vens <vensalex1991@gmail.com>
2026-07-17 11:04:47 +0200

    Add dependance

--- app/Http/Controllers/Api/ArchiveController.php
+++ app/Http/Controllers/Api/ArchiveController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-
+use App\Models\Task;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 class ArchiveController extends Controller
 {
-    //
+
 }
